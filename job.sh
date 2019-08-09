f0dd90bb6cbeb96fe47a370a65b04fd1790554fb
garciadd <garciad@ifca.unican.es>
2019-08-09 10:20:30 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,1 +1,1 @@
-python3 /home/jovyan/xdc_lfw_sat/sat_server/xdc_lfw_sat.py -sd $START_DATE -ed $END_DATE --region $REGION -sat $SAT
+python3 xdc_lfw_sat/sat_server/xdc_lfw_sat.py -sd $START_DATE -ed $END_DATE --region $REGION -sat $SAT -path $SAT_PATH
