188e7db7d92f2a4b13473db78ad6b6672f07a120
Fernando Aguilar <ferag@live.com>
2019-08-09 10:36:40 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,1 +1,11 @@
+env
+mkdir -p /onedata/output
+
+ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o rw /onedata/output || exit 1
+
+echo Start at $(date)
+
+OUTPUTDIR="/onedata/output/$ONEDATA_SPACE/$MODEL_PATH"
 python3 xdc_lfw_sat/sat_server/xdc_lfw_sat.py -sd $START_DATE -ed $END_DATE --region $REGION -sat $SAT -path $SAT_PATH
+mv "$SAT_PATH" "$OUTPUTDIR"
+echo End at $(date)
