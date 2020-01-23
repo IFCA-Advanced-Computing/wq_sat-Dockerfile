ddc2ad1639899dbca99f981e301183e5e87acacf
garciadd <garciad@ifca.unican.es>
2020-01-23 13:38:13 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -5,7 +5,11 @@ ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$ONEDATA
 
 echo Start at $(date)
 
-OUTPUTDIR=/onedata/output/$ONEDATA_SPACE/
-python3 xdc_lfw_sat/sat_server/xdc_lfw_sat.py -sd $START_DATE -ed $END_DATE -reg $REGION --coord $COORD -sat $SAT -path $OUTPUTDIR
+OUTPUTDIR="/onedata/output/$ONEDATA_SPACE/"
+#python3 xdc_lfw_sat/sat_server/xdc_lfw_sat.py -sd $START_DATE -ed $END_DATE -reg $REGION --coord $COORD -sat $SAT -path "$OUTPUTDIR"
+
+# Run
+$python3 xdc_lfw_sat/sat_server/xdc_lfw_sat.py -sd $START_DATE -ed $END_DATE -reg $REGION --coord $COORD -sat $SAT -path "$OUTPUTDIR" $argfile > "$OUTPUTDIR"/satellite_output.log
+
 #mv  "$SAT_PATH"/ "$OUTPUTDIR"
 echo End at $(date)
