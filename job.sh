bee3a198b36229c119df62a8cc7723e7d4aa0746
garciadd <garciad@ifca.unican.es>
2020-05-20 18:05:50 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,12 +1,10 @@
 env
 mkdir -p /onedata/output
 
-ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" ONECLIENT_PROVIDER_HOSTNAME="$ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o rw /onedata/output || exit 1
+ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" ONECLIENT_PROVIDER_HOSTNAME="$ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o rw "$ONEDATA_MOUNT_POINT" || exit 1
 
 echo Start at $(date)
-
-OUTPUTDIR="/onedata/output/$ONEDATA_SPACE/$REGION/"
-#OUTPUTDIR=$SAT_PATH
+OUTPUTDIR="$ONEDATA_MOUNT_POINT/$ONEDATA_SPACE/$REGION/"
 
 # Run
 python3 wq_sat/wq_server/main.py -sat_args $SAT_ARGS -path "$OUTPUTDIR" $argfile > "$OUTPUTDIR"/satellite_"$START_DATE"_"$END_DATE"_output.log
